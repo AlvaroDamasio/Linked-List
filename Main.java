031e3de0f8bfc2287483d55f6614fc6f5a5d6eb0
Alvaro <alvarod918@gmail.com>
2024-01-25 15:04:37 -0300

    create a main class to test the list

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleLinkedList list = new SimpleLinkedList();
+        list.adicionar(1);
+        list.adicionar(2);
+        list.adicionar(3);
+        list.adicionar(4);
+        
+        list.limpar();
+
+        list.percorrer();
+
+    }
+}
